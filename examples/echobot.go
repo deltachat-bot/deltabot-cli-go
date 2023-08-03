9fa8ea0f450a2af712620a1a326a9fa3dc84e672
adbenitez <asieldbenitez@gmail.com>
2023-08-03 05:29:14 +0200

    move info subcommand to infobot example

--- examples/echobot.go
+++ examples/echobot.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/deltachat-bot/deltabot-cli-go/botcli"
 	"github.com/deltachat/deltachat-rpc-client-go/deltachat"
 	"github.com/spf13/cobra"
@@ -10,25 +8,6 @@ import (
 
 func main() {
 	cli := botcli.New("echobot")
-
-	// add an "info" CLI subcommand as example
-	infoCmd := &cobra.Command{
-		Use:   "info",
-		Short: "display information about the Delta Chat core running in this system or about an specific account if one was selected with -a/--account",
-		Args:  cobra.ExactArgs(0),
-	}
-	cli.AddCommand(infoCmd, func(cli *botcli.BotCli, bot *deltachat.Bot, cmd *cobra.Command, args []string) {
-		var info map[string]string
-		if cli.SelectedAddr == "" { // no account selected with --a/--account, show system info
-			info, _ = bot.Rpc.GetSystemInfo()
-		} else { // account selected, show info about that account
-			accId, _ := cli.GetAccount(bot.Rpc, cli.SelectedAddr)
-			info, _ = bot.Rpc.GetInfo(accId)
-		}
-		for key, val := range info {
-			fmt.Printf("%v=%#v\n", key, val)
-		}
-	})
 
 	// incoming message handling
 	cli.OnBotInit(func(cli *botcli.BotCli, bot *deltachat.Bot, cmd *cobra.Command, args []string) {
--- examples/infobot.go
+++ examples/infobot.go
@@ -5,6 +5,8 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/deltachat-bot/deltabot-cli-go/botcli"
 	"github.com/deltachat/deltachat-rpc-client-go/deltachat"
 	"github.com/spf13/cobra"
@@ -37,6 +39,25 @@ func onNewMsg(bot *deltachat.Bot, accId deltachat.AccountId, msgId deltachat.Msg
 }
 
 func main() {
+	// add an "info" CLI subcommand as example
+	infoCmd := &cobra.Command{
+		Use:   "info",
+		Short: "display information about the Delta Chat core running in this system or about an specific account if one was selected with -a/--account",
+		Args:  cobra.ExactArgs(0),
+	}
+	cli.AddCommand(infoCmd, func(cli *botcli.BotCli, bot *deltachat.Bot, cmd *cobra.Command, args []string) {
+		var info map[string]string
+		if cli.SelectedAddr == "" { // no account selected with --a/--account, show system info
+			info, _ = bot.Rpc.GetSystemInfo()
+		} else { // account selected, show info about that account
+			accId, _ := cli.GetAccount(bot.Rpc, cli.SelectedAddr)
+			info, _ = bot.Rpc.GetInfo(accId)
+		}
+		for key, val := range info {
+			fmt.Printf("%v=%#v\n", key, val)
+		}
+	})
+
 	cli.OnBotInit(func(cli *botcli.BotCli, bot *deltachat.Bot, cmd *cobra.Command, args []string) {
 		bot.OnNewMsg(onNewMsg)
 	})
