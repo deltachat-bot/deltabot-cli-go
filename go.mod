1987c972446bc8aede4e3254f6931888c1fa5f28
adbenitez <asieldbenitez@gmail.com>
2024-02-04 19:29:26 -0500

    chore: update deps

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/deltachat-bot/deltabot-cli-go
 go 1.21
 
 require (
-	github.com/deltachat/deltachat-rpc-client-go v1.127.0
+	github.com/deltachat/deltachat-rpc-client-go v1.134.0
 	github.com/mdp/qrterminal/v3 v3.0.0
 	github.com/spf13/cobra v1.6.1
 	github.com/stretchr/testify v1.8.2
@@ -11,8 +11,8 @@ require (
 )
 
 require (
-	github.com/creachadair/jrpc2 v1.0.0 // indirect
-	github.com/creachadair/mds v0.0.1 // indirect
+	github.com/creachadair/jrpc2 v1.1.2 // indirect
+	github.com/creachadair/mds v0.8.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -20,7 +20,7 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	rsc.io/qr v0.2.0 // indirect
 )
--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/env bash
 
+echo "Checking code with gofmt..."
 OUTPUT=`gofmt -d .`
 if [ -n "$OUTPUT" ]
 then
@@ -7,12 +8,13 @@ then
     exit 1
 fi
 
+echo "Checking code with golangci-lint..."
 if ! command -v golangci-lint &> /dev/null
 then
+    echo "golangci-lint not found, installing..."
     # binary will be $(go env GOPATH)/bin/golangci-lint
-    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.52.2
+    curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.55.2
 fi
-
 
 if ! golangci-lint run
 then
@@ -21,19 +23,22 @@ fi
 
 if ! command -v deltachat-rpc-server &> /dev/null
 then
-    echo "deltachat-rpc-server could not be found, installing..."
-    pip3 install deltachat-rpc-server
+    echo "deltachat-rpc-server not found, installing..."
+    curl -L https://github.com/deltachat/deltachat-core-rust/releases/latest/download/deltachat-rpc-server-x86_64-linux --output deltachat-rpc-server
+    chmod +x deltachat-rpc-server
+    export PATH=`pwd`:"$PATH"
 fi
 
 if ! command -v courtney &> /dev/null
 then
-    echo "courtney could not be found, installing..."
-    go install github.com/dave/courtney@latest
+    echo "courtney not found, installing..."
+    go install github.com/dave/courtney@master
 fi
 
 # test examples
 for i in ./examples/*.go
 do
+    echo "Testing examples: $i"
     if ! go build -v "$i"
     then
         exit 1
